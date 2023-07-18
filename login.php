5cb0da8666995dfdee9f6fdd19122ab757b102da
ViverIT1 <casajanjosephviver@gmail.com>
2023-07-18 19:02:34 +0800

    major changes

--- login.php
+++ login.php
@@ -1,6 +1,29 @@
 <?php
-    $firstname = $_POST['$firstName'];
-    $password = $_POST['$passWord'];
+ob_start();
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+$username = $_POST['username'];
+$password = $_POST['password'];
 
-    $conn = new mysqli('localhost', 'root','','webinventorydb');
+$conn = new mysqli('localhost', 'root', '', 'webinventorydb');
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$stmt = $conn->prepare("SELECT * FROM userauth WHERE user_name = ? AND user_password = ?");
+$stmt->bind_param("ss", $username, $password);
+$stmt->execute();
+$result = $stmt->get_result();
+echo "Number of rows: " . mysqli_num_rows($result);
+
+if (mysqli_num_rows($result) == 1) {
+    session_start();
+    $_SESSION["username"] = $username;
+    header("Location: http://localhost/yourdirectory/Homepage.html");
+    echo "<p>Success Login</p>";
+} else {
+    echo "<p>Incorrect username or password.</p>";
+}
+
+mysqli_close($conn);
 ?>
