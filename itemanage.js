093ce381849dfc0a065660ed9a20568f5107c3ba
ViverIT1 <casajanjosephviver@gmail.com>
2023-07-26 00:38:41 +0800

    PopWindowForAdd

---
+++ itemanage.js
@@ -0,0 +1,8 @@
+function openAddWindow(){
+    let openaddpop = document.getElementById("openaddpop");
+    openaddpop.classList.add("openAddpop");
+}
+function closeAddWindow(){
+    let openaddpop = document.getElementById("openaddpop");
+    openaddpop.classList.remove("openAddpop");
+}
