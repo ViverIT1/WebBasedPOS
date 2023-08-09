221fedb2a91d3510e8c03cb861311a1a255829b5
ViverIT1 <casajanjosephviver@gmail.com>
2023-08-09 19:19:18 +0800

    UpdateSomeBackends

--- ItemAdd.php
+++ ItemAdd.php
@@ -1,5 +1,5 @@
 <?php
-include_once('PhpCon.php');
+include_once('PhpCon.php'); // Make sure the database connection is included
 require 'vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\IOFactory;
@@ -13,47 +13,75 @@ if (isset($_REQUEST['import-excel'])) {
         $worksheet = $obj->getActiveSheet();
         $data = $worksheet->toArray();
 
+        $successCount = 0; // Counter for successfully inserted records
+
         // Start from the second row (index 1) to skip the header row
         for ($rowIndex = 1; $rowIndex < count($data); $rowIndex++) {
             $row = $data[$rowIndex];
 
             // Process the data from the Excel file
-            $Product_Description = $row['2'];
-            $Product_ID = $row['0'];
-            $Product_Name = $row['1'];
-            $Product_Category = $row['3'];
-            $Product_Price = $row['4'];
-            $Product_Maximum = $row['10'];
-            $Product_Quantity = $row['5'];
-            $Product_Reorder = $row['8'];
-            $Product_Barcode = $row['6'];
-            $Product_Expiry = $row['7'];
-            $Product_Minimum = $row['9'];
-            // Check if the ID already exists in the database
+            $Product_Name = $row[0]; // Product Name
+            $Product_Description = $row[1]; // Description
+            $Product_Category = $row[2]; // Category
+            $Product_Price = $row[3]; // Price
+            $Product_Quantity = $row[4]; // Quantity
+            $Product_Barcode = $row[5]; // Barcode
+            $Product_Expiry = $row[6]; // Expiry Date (yyyy-mm-dd)
+            $Product_Reorder = $row[7]; // Reorder point
+            $Product_Maximum = $row[8] ?? 0; // Maximum Stock Level (use 0 as default if missing)
+            $Product_Minimum = $row[9]; // Minimum Stock Level
+
+
+            // Check if the product already exists
             $existingQuantity = 0;
-            $result = mysqli_query($conn, "SELECT pro_quantity FROM itemlist WHERE pro_ID = '$Product_ID'");
-            if (mysqli_num_rows($result) > 0) {
-                $existingData = mysqli_fetch_assoc($result);
-                $existingQuantity = $existingData['pro_quantity'];
+            $result = mysqli_query($conn, "SELECT pro_quantity FROM itemlist WHERE pro_name = '$Product_Name'");
+            
+            if ($result !== false) { // Check if query was successful
+                if (mysqli_num_rows($result) > 0) { // Check if there are rows returned
+                    $existingData = mysqli_fetch_assoc($result);
+                    $existingQuantity = $existingData['pro_quantity'];
+                } else {
+                    // Product does not exist, handle this case if needed
+                    // For now, you can set $existingQuantity to 0 or any other appropriate value
+                    $existingQuantity = 0;
+                }
+            } else {
+                echo "Query error: " . mysqli_error($conn);
             }
 
-            // If the ID exists, update the quantity; otherwise, insert a new record
+            // Insert or update data based on existence
             if ($existingQuantity > 0) {
                 $updatedQuantity = $existingQuantity + $Product_Quantity;
-                mysqli_query($conn, "UPDATE itemlist SET pro_quantity = '$updatedQuantity' WHERE pro_ID = '$Product_ID'");
-            } else {
-                $sql = "INSERT INTO itemlist (pro_inf, pro_ID, pro_name, pro_cat, pro_price, pro_maxStock, pro_quantity, pro_reorder, pro_barcode, pro_exp, pro_minStock) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-                $stmt = $conn->prepare($sql);
-                $stmt->bind_param("sssssiiiiii", $Product_Description, $Product_ID, $Product_Name, $Product_Category, $Product_Price, $Product_Maximum, $Product_Quantity, $Product_Reorder, $Product_Barcode, $Product_Expiry, $Product_Minimum);
-                $stmt->execute();
-                    }        
+                $updateQuery = "UPDATE itemlist SET pro_quantity = '$updatedQuantity' WHERE pro_name = '$Product_Name'";
+                if (mysqli_query($conn, $updateQuery)) {
+                    $successCount++;
+                } else {
+                    echo "Update error: " . mysqli_error($conn);
                 }
+            } else {
+                $insertQuery = "INSERT INTO itemlist (pro_inf, pro_name, pro_cat, pro_price, pro_maxStock, pro_quantity, pro_reorder, pro_barcode, pro_exp, pro_minStock) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                $stmt = $conn->prepare($insertQuery);
 
-            echo "Data Inserted Successfully";
+                if ($stmt) {
+                    $stmt->bind_param("sssssiisii", $Product_Description, $Product_Name, $Product_Category, $Product_Price, $Product_Maximum, $Product_Quantity, $Product_Reorder, $Product_Barcode, $Product_Expiry, $Product_Minimum);
+                    if ($stmt->execute()) {
+                        $successCount++;
+                    } else {
+                        echo "Insert error: " . $stmt->error;
+                    }
+                    $stmt->close();
+                } else {
+                    echo "Prepare error: " . $conn->error;
+                }
+            }
+        }
+
+        echo "$successCount records inserted or updated successfully";
     } else {
         echo "Invalid file format. Only XLSX, XLS, and CSV files are allowed.";
     }
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -72,9 +100,6 @@ if (isset($_REQUEST['import-excel'])) {
     <br>
             <div class="form-box">
                 <form action="ItemAddHandler.php" method="post">
-                    <label for="Product_ID">Product ID:</label>
-                    <input type="text" name="Product_ID" id="Product_ID" required>
-                    <br>
                     <label for="Product_Name">Product Name:</label>
                     <input type="text" name="Product_Name" id="Product_Name" required>
                     <br>
--- ItemAddHandler.php
+++ ItemAddHandler.php
@@ -1,5 +1,4 @@
 <?php
-$Product_ID = $_POST['Product_ID'];
 $Product_Name = $_POST['Product_Name'];
 $Description = $_POST['Description'];
 $Category = $_POST['Category'];
@@ -17,19 +16,23 @@ if ($conn->connect_error) {
 }
 
 $sql = "INSERT INTO itemlist 
-        (pro_ID, pro_name, pro_inf, pro_cat, pro_price, pro_quantity, pro_barcode, pro_exp, pro_reorder, pro_minStock, pro_maxStock) 
+        (pro_name, pro_inf, pro_cat, pro_price, pro_quantity, pro_barcode, pro_exp, pro_reorder, pro_minStock, pro_maxStock) 
         VALUES 
-        (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
 $stmt = mysqli_prepare($conn, $sql);
 
-mysqli_stmt_bind_param($stmt, "sssssiiiiii", $Product_ID, $Product_Name, $Description, $Category, $Price, $Quantity, $Barcode, $Expiry_Date, $Reorder_Point, $Minimum, $Maximum);
+mysqli_stmt_bind_param($stmt, "sssssiiiii", $Product_Name, $Description, $Category, $Price, $Quantity, $Barcode, $Expiry_Date, $Reorder_Point, $Minimum, $Maximum);
 
 if (mysqli_stmt_execute($stmt)) {
-    header('location:ItemAdd.php');
+    header('location: ItemAdd.php');
 } else {
     echo "Error: " . mysqli_error($conn);
 }
+
+mysqli_stmt_close($stmt);
+mysqli_close($conn);
+
 
 mysqli_stmt_close($stmt);
 mysqli_close($conn);
