4a078b38d0046b11c00bcd4b2427d88c4100b72d
raaanyel14 <ranielperez14@gmail.com>
2023-08-12 16:29:45 +0800

    i change the href to packagelist.html

--- next.php
+++ next.php
@@ -35,7 +35,7 @@ if (!isset($_SESSION['username'])) {
       <a href="itemanage.php" target="MainIframe" class="sub-module">Item Management</a>
       <a href="stockadj.php" target="MainIframe" class="sub-module">Stock Adjusment</a>
       <a href="discount.php" target="MainIframe" class="sub-module">Discount</a>
-      <a href="packages.html" target="MainIframe" class="sub-module">Packages</a>
+      <a href="packagelist.html" target="MainIframe" class="sub-module">Packages</a>
     </div>
     </div>
 
