949a9ef3683c70ad5b538ce29b34901c18353a49
raaanyel14 <ranielperez14@gmail.com>
2023-08-17 19:45:55 +0800

    add iframe for cashiring

--- cashiering.php
+++ cashiering.php
@@ -30,47 +30,6 @@ $result = $conn->query($query);
     </form>
 </div>
 
-<table class="content-table">
-    <thead>
-        <tr>
-            <th>Product ID</th>
-            <th>Product</th>
-            <th>Quantity</th>
-            <th>Unit Price</th>
-            <th>Total</th>
-            <th>Action</th>
-        </tr>
-    </thead>
-    <tbody>
-    <?php
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $product_id = $row["pro_ID"];
-            $product_name = $row["pro_name"];
-            $product_quantity = $row["pro_quantity"];
-            $product_price = $row["pro_price"];
-            $total_price = $row["pro_total"];
-    ?>
-            <tr>
-                <td rowspan="1" class="input-cell"><input type="text" value="<?php echo $product_id ?>" readonly></td>
-                <td class="input-cell"><input type="text" value="<?php echo $product_name ?>" readonly></td>
-                <td class="input-cell"><input type="number" value="<?php echo $product_quantity ?>"></td>
-                <td class="input-cell"><input type="number" value="<?php echo $product_price ?>" readonly></td>
-                <td class="input-cell"><input type="number" value="<?php echo $total_price ?>" readonly></td>
-                <td class="input-cell">
-                    <form method="post" action="cashierRemoveProduct.php">
-                        <input type="hidden" name="product_id" value="<?php echo $product_id ?>">
-                        <button type="submit">Remove</button>
-                    </form>
-                </td>
-            </tr>
-    <?php
-        }
-    }
-    ?>
-</tbody>
-</table>
-
 <div class="scnd_box">
     <div class="scnd_container">
         <div class="subtotal">
@@ -110,6 +69,11 @@ $result = $conn->query($query);
                     <input type="hidden" name="amount_change" value="0">
                  </div>
 </div>
+</div>
+
+
+<div class="iframe-cashiering">
+<iframe src="cashieringiframe.php"></iframe>
 </div>
 
 <button id="popupButton" type="button">Settle Payment</button>
