17eaee7dfa44613aa6cabe76e4704e06ca5dcaa8
raaanyel14 <ranielperez14@gmail.com>
2023-10-08 19:13:46 +0800

    change entire css code of interface

--- next.php
+++ next.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['username'])) {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Sales and Inventory Management System</title>
-    <link rel="stylesheet" href="nextstyle.css">
+    <link rel="stylesheet" href="nextsyle.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <script src="next.js"></script>
 
