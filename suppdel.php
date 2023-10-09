6a2c698f00b92b4b1d8286bcb01db159bb27f163
Jaxxtin <marcjustinsalastenorio@gmail.com>
2023-10-09 17:58:56 +0800

    added some classes on supplier delivery

--- suppdel.php
+++ suppdel.php
@@ -28,6 +28,7 @@ if ($supdeliveryResult->num_rows > 0) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Supplier Delivery</title>
+    <link rel="stylesheet" type="text/css" href="suppdel.css">
 </head>
 <body>
     <h1>Supplier Delivery</h1>
@@ -35,32 +36,32 @@ if ($supdeliveryResult->num_rows > 0) {
     <form method="post" action="suppdelHandler.php"> 
         <label>Input PO# to deliver:</label>
         <input type="text" name="SuppPo" id="SuppPo" autocomplete="off">
-        <input type="submit" value="Create supdelivery table">
+        <input class="action-button approve-button" type="submit" value="Create supdelivery table">
     </form>
 
     <?php foreach ($supdeliveryData as $supdelPono => $data): ?>
-        <table>
+        <table class= "order-table">
             <thead>
                 <tr>
-                    <th>Details</th>
-                    <th colspan="3">Action</th>
+                    <th class= "details-header">Details</th>
+                    <th colspan="3" class= "action-header">Action</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <td>Supplier Name:<?php echo $data['supdelSupp']; ?></td>
+                    <td class= "supplier-name">Supplier Name:<?php echo $data['supdelSupp']; ?></td>
                     <td>
                         <form method="post" action="suppdeltoinv.php">
                             <input type="hidden" name="pro_PONO" value="<?php echo $supdelPono; ?>">
-                            <button type="submit">Delivered</button>
+                            <button type="submit" class="action-button approve-button">Delivered</button>
                         </form>
                     </td>
-                    <td><a><button>Details</button></a></td>
-                    <td><a><button>Cancelled</button></a></td>
+                    <td><a><button class= "action-button details-button">Details</button></a></td>
+                    <td><a><button class= "action-button delete-button">Cancelled</button></a></td>
                 </tr>
                 <tr>
-                    <td>PO#:<?php echo $supdelPono; ?></td>
-                    <td colspan="3">Status:</td>
+                    <td class="po-number">PO#:<?php echo $supdelPono; ?></td>
+                    <td colspan="3" class= "status">Status:</td>
                 </tr>
             </tbody>
         </table>
