b5f255a7908c9ce221d04bc5cf32f19b18c2e9cb
ViverIT1 <casajanjosephviver@gmail.com>
2023-10-16 11:29:05 +0800

    CashieringInputAndParticalCal

--- cashieringRetrieve.php
+++ cashieringRetrieve.php
@@ -3,22 +3,29 @@ include("PhpCon.php"); // Include your database connection script
 
 $productID = $_GET['productID']; // Get the Product ID from the query string
 
-$sql = "SELECT pro_ID, pro_name, pro_quantity, pro_price FROM itemlist WHERE pro_ID = $productID";
+// Use a prepared statement to prevent SQL injection
+$sql = "SELECT pro_IDQR, pro_name, pro_quantity, pro_price FROM itemlist WHERE pro_IDQR = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $productID); // Assuming pro_IDQR is a string
 
-$result = $conn->query($sql);
+if ($stmt->execute()) {
+    $result = $stmt->get_result();
 
-$products = [];
-
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        $products[] = $row;
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $products[] = $row;
+        }
+        // Product found, return the JSON data
+        echo json_encode($products);
+    } else {
+        // Product not found, return a JSON message
+        $response = array('message' => 'Product not found');
+        echo json_encode($response);
     }
+} else {
+    // Handle the case when the SQL query fails
+    $response = array('message' => 'Error in SQL query');
+    echo json_encode($response);
 }
 
-// Close the database connection
-$conn->close();
-
-// Send the product data as JSON
-header("Content-Type: application/json");
-echo json_encode($products);
 ?>
