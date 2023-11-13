704096360451b30a03acf05301aa1d37b931021a
raaanyel14 <ranielperez14@gmail.com>
2023-11-14 00:51:56 +0800

    change href to itemadd.css

--- ItemUpSet.php
+++ ItemUpSet.php
@@ -26,7 +26,7 @@ if (isset($_GET['UpSetID'])) {
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" type="text/css" href="itemanage.css">
+        <link rel="stylesheet" type="text/css" href="itemAdd.css">
         <title>Item Update</title>
     </head>
     <body>
