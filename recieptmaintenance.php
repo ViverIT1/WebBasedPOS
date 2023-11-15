f8c81628febb05d5da9196f896cd5fa3ca539533
raaanyel14 <ranielperez14@gmail.com>
2023-11-16 00:16:18 +0800

    minor title

--- recieptmaintenance.php
+++ recieptmaintenance.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Receipt Maintenance</title>
+    <title>Temporary Receipt Maintenance</title>
     <link rel="stylesheet" type="text/css" href="recieptmaintenance.css">
 </head>
 <body>
